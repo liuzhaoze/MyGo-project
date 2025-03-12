d93bd02e6e149dd03f55fd55d1d5ba7fc7aaad23
liuzhaoze <1045954863@qq.com>
2025-03-12 13:42:06 +0800

    创建死信队列

--- internal/common/broker/rabbitmq.go
+++ internal/common/broker/rabbitmq.go
@@ -9,6 +9,11 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+const (
+	DLX = "dlx"
+	DLQ = "dlq"
+)
+
 func Connect(user, password, host, port string) (*amqp.Channel, func() error) {
 	address := fmt.Sprintf("amqp://%s:%s@%s:%s/", user, password, host, port)
 	conn, err := amqp.Dial(address)
@@ -31,7 +36,30 @@ func Connect(user, password, host, port string) (*amqp.Channel, func() error) {
 		logrus.Fatal(err)
 	}
 
+	if err := createDLX(ch); err != nil {
+		logrus.Fatal(err)
+	}
 	return ch, conn.Close
+}
+
+func createDLX(ch *amqp.Channel) error {
+	q, err := ch.QueueDeclare("share_queue", true, false, false, false, nil)
+	if err != nil {
+		return err
+	}
+
+	err = ch.ExchangeDeclare(DLX, "fanout", true, false, false, false, nil)
+	if err != nil {
+		return err
+	}
+
+	err = ch.QueueBind(q.Name, "", DLX, false, nil)
+	if err != nil {
+		return err
+	}
+
+	_, err = ch.QueueDeclare(DLQ, true, false, false, false, nil)
+	return err
 }
 
 type RabbitMQHeaderCarrier map[string]interface{}
